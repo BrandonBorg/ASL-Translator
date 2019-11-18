e69023302cdb1b0b831a1669e5889341038fcee8
Brandon Borg <brandonjamesborg@gmail.com>
2019-11-18 17:19:49 -0500

    Fixed the test set structure

--- code/DataImport.py
+++ code/DataImport.py
@@ -3,7 +3,7 @@ import os
 import csv
 from PIL import Image
 
-size = 48,48
+size = 48, 48
 
 
 # Accepts int, returns string value of corresponding class name
@@ -31,3 +31,15 @@ def img_to_csv(dir_name, csv_name):
             with open(csv_name, "a", newline='') as fp:
                 wr = csv.writer(fp)
                 wr.writerow(img)
+
+
+def get_data(csv_name):
+    num_classes = 29
+    num_inputs = 48 ** 48
+
+    data_set = np.loadtxt(csv_name, delimiter=',')
+
+    x_data = data_set[:, 0:num_inputs]  # extract the pixel values
+    y_data = data_set[:, num_inputs:num_inputs + num_classes] # extract the class
+
+    return [x_data, y_data]
--- code/Main.py
+++ code/Main.py
@@ -1,9 +1,11 @@
 import DataImport
 
 train_dir_name = '../asl_alphabet_train/asl_alphabet_train'
-train_csv_name = "../asl_alphabet_train/asl_alphabet_train.csv"
+train_csv_name = '../asl_alphabet_train/asl_alphabet_train.csv'
+
 test_dir_name = '../asl_alphabet_test/asl_alphabet_test'
-test_csv_name = "../asl_alphabet_test/asl_alphabet_test.csv"
+test_csv_name = '../asl_alphabet_test/asl_alphabet_test.csv'
 
 #Uncomment to create csv
-#DataImport.img_to_csv(train_dir_name, train_csv_name)
+#DataImport.img_to_csv(test_dir_name, test_csv_name)
+
